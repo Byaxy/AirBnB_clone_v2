d78c9ebe07571b81fa4c143505b5643db810ce16
Byaxy <charlesbyaxy@gmail.com>
2022-02-05 15:24:15 +0000

    fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,10 +3,54 @@
 starts a Flask web application
 """
 
-from flask import Flask, render_template
-from models import *
+from flask import Flask, escape, render_template
 from models import storage
+from models.state import State
+
+
 app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    '''returns a hello message'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''returns a hello message'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def ctext(text):
+    '''returns a test message'''
+    return 'C {}'.format(escape(text).replace('_', ' '))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythontext(text='is cool'):
+    '''returns a test message'''
+    return 'Python {}'.format(escape(text).replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def numberRoute(n):
+    '''returns a test message'''
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def templateRoute(n):
+    '''returns a test template'''
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def template_odd_even_Route(n):
+    '''returns a test template'''
+    return render_template('6-number_odd_or_even.html', n=n)
 
 
 @app.route('/states_list', strict_slashes=False)
